d1b1130be9f93bc2f03d1dfcbd659c6c1d951449
Gerard Noordhuis <gerardnoordhuis@gmail.com>
2019-01-23 16:45:54 +0000

    Mooi

---
+++ Project_Filmsite/README.md
@@ -0,0 +1,132 @@
+# nederlandsefilm
+## Isa-Ali Kirca &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 12014672
+## Gerard Noordhuis &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 11919582
+## Patrick Smit &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 11433604
+
+### [Schetsen](https://docs.google.com/presentation/d/1Dk9pYlrxR6hi45ncdenz7bOs3y8t3Wdz3CnpoxP1xaI/edit?usp=sharing)
+Bekijk de concepten/schetsen op Google Slides^.
+
+### Samenvatting:
+
+  * Met NederlandseFilms.nl willen wij een site maken die gebruikers de optie geeft om hun favoriete Nederlandse films op te zoeken. Verder kunnen gebruikers kijklijsten creëren en vrienden maken met andere gebruikers, om vervolgens lijsten met elkaar te delen. Gebruikers kunnen ook likes geven aan films en comments achterlaten. 
+
+### Features
+
+Voor ingelogde gebruikers
+  * 1.1 Gebruikersinteractie/ Vrienden op het platform
+    * Anderen toevoegen a.d.h.v. gebruikersnaam (accepteren, weigeren)
+    * Vrienden berichten sturen
+    * Filmtitel delen met een vriend (in de eerder genoemde chat?)
+    * Films toevoegen aan een gezamenlijke lijst (update 2 lijsten)  
+    * Lijst met top gebruikers bekijken 
+    * Mensen volgen
+    * Kijken welke films je met vrienden ‘in common’ hebt
+
+
+* 1.2 Eigen account
+  * 1.2.1 Account informatie
+   * Vul account details in (wachtwoord, e-mail, gebruikersnaam, geslacht)
+   * Valideer account
+   * Koppel Social Media accounts met NLfilms account
+   * Profiel bewerken (wachtwoord, e-mail, gebruikersnaam? geslacht? verander koppeling met social media)
+  * 1.2.2 Films opslaan etc.
+   * Films toevoegen aan een lijst met favorieten
+   * Film toevoegen aan een kijklijst
+   * Film toevoegen aan ‘check-in’ of historie
+   * Een rating toevoegen aan TMDb
+
+* 1.3 Pagina van film:
+  * voeg film toe aan kijklijst
+  * Samenvatting / Omschrijving
+  * Weergeef de rating vanuit de API
+  * Bekijk comments van gebruikers
+  * (wanneer ingelogd:)
+    * Schrijf een comment (over de film)
+    * Geef een rating (wordt dorgestuurd naar TMDb)
+
+
+* 1.4 Homepage:
+  * 1.4.1 Homepagina: Layout
+    * Item
+    * Omschrijving
+    * Zoekbalk
+  * 1.4.2 Homepagina: Zoeken:
+    * film → pagina met informatie (zie Minimum viable product)
+    * acteur → pagina met de best beoordeelde films van de desbetreffende acteur
+    * jaar → pagina met de best beoordeelde films van dat jaar
+
+* 1.5 Account:
+  * maak een account aan
+  * (wanneer ingelogd:)
+    * Likes
+    * Friend request (om naar elkaars accounts te kijken)
+    
+* Mogelijke extra’s
+  * Delen via Whatsapp, Facebook, enz.
+
+Wat we niet doen: 
+  * Eigen ratings, beoordelingen et cetera, omdat IMDb en TMDb hierin niet te overtreffen zijn in het aantal relevante beoordelingen.
+  
+  
+### Minimum viable product:
+  * Er moet gezocht kunnen worden naar een specifieke film. Wanneer de film gevonden wordt, omvat de getoonde pagina in ieder geval:
+    * titel van de film
+    * jaar van uitkomst in theaters
+    * speelduur
+    * regisseur(s)
+    * genre
+    * acteurs
+    * Eventueel wordt er ook een poster/afbeelding/omzet en een beoordeling bijgevoegd.
+
+  * Het zou mooi zijn als het ook mogelijk is om per categorie de beste films te tonen. Het kan zijn dat  dit niet real-time mogelijk is, omdat veel film-API’s de beperking hebben dat er een maximum aantal data-aanvragen per tijdseenheid wordt gehanteerd. 
+
+### Afhankelijkheden
+#### Eventueel te gebruiken API’s:
+
+The Movie Database(TMDb):
+https://www.themoviedb.org/documentation/api 
+
+Filmtotaal:
+http://api.filmtotaal.nl/ 
+
+IMDb Scraper (Third party):
+http://imdbpy.sourceforge.net/
+
+TMDb Wrapper (Third party):
+https://pypi.org/project/tmdbsimple/ 
+https://github.com/celiao/tmdbsimple
+
+#### Verdere sites en plug-ins:
+
+Bootstrap: 
+https://getbootstrap.com/docs/4.2/getting-started/introduction/
+
+Bootsnipp: 
+https://bootsnipp.com/
+
+#### Concurerende sites:
+
+https://www.imdb.com:
+ * Niet gelimiteerd tot Nederlandse films, maar heeft ze wel.
+ * Bindingen met bedrijven als Amazon
+ * Heeft pagina's met nieuws over de filmindustrie.
+ * Behandelt ook Video Games
+ 
+https://www.tmdb.com:
+ * Heeft links naar de films die momenteel in de bioscoop draaaien.
+ * Internetfora over films
+
+ https://www.filmvandaag.nl:
+  * Heeft links naar de films die momenteel in de bioscoop draaaien.
+  * Heeft links naar de series/films die te zien zijn op Netfilx
+  * Geeft releases op dvd/bluray aan.
+  
+ 
+#### Moeilijke delen:
+Veel code zal het werk complexer maken. De zaken waarvan we momenteel verwachten dat ze lastig zullen worden:
+* Andere gebruikers toevoegen a.d.h.v. gebruikersnaam
+* Vrienden berichten sturen
+* Filmtitels delen met een vriend
+* Films toevoegen aan een gezamenlijke lijst (update 2 lijsten)  
+* Lijst met top gebruikers bekijken 
+* Mensen volgen
---
+++ Project_Filmsite/applicationoud.py
@@ -0,0 +1,209 @@
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask_session import Session
+from passlib.apps import custom_app_context as pwd_context
+from tempfile import mkdtemp
+import datetime, requests, json, xml.etree.ElementTree, urllib
+from helpers import *
+
+# configure application
+app = Flask(__name__)
+
+# ensure responses aren't cached
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
+# custom filter
+app.jinja_env.filters["usd"] = usd
+
+# configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# configure CS50 Library to use SQLite database
+db = SQL("sqlite:///nlfilms.db")
+
+@app.route("/index")
+@login_required
+def index():
+    from urllib.request import urlopen
+    popular = json.loads(str((requests.get("https://api.themoviedb.org/3/discover/movie?api_key=9c226374f10b2dcd656cf7c348ee760a&language=nl&sort_by=popularity.desc&page=1&with_original_language=nl").content).decode('UTF-8')))
+    popular_results = popular["results"]
+    return render_template("index.html", results=popular_results)
+
+@app.route("/")
+def homepage():
+    # Haal populaire films op
+    from urllib.request import urlopen
+    popular = json.loads(str((requests.get("https://api.themoviedb.org/3/discover/movie?api_key=9c226374f10b2dcd656cf7c348ee760a&language=nl&sort_by=popularity.desc&page=1&with_original_language=nl").content).decode('UTF-8')))
+    popular_results = popular["results"]
+
+    # Vanavond op televisie
+    return render_template("homepage.html", results=popular_results)
+
+@app.route("/wachtwoord")
+def wachtwoord():
+    return render_template("wachtwoord.html")
+
+@app.route("/overons")
+def overons():
+    return render_template("overons.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user."""
+    if request.method == "POST":
+
+        # ensure username was submitted
+        if not request.form.get("gebruikersnaam"):
+            return apology("Geef een gebruikersnaam op.")
+
+        # ensure password was submitted
+        elif not request.form.get("email"):
+            return apology("Geef een e-mailadres op.")
+
+        # ensure password was submitted
+        elif not request.form.get("wachtwoord"):
+            return apology("Geef een wachtwoord op.")
+
+        # ensure password-confirmation was submitted
+        elif not request.form.get("wachtwoord-confirmatie"):
+            return apology("Geef het wachtwoord nogmaals op.")
+
+        # ensure password equals password confirmation
+        if request.form.get("wachtwoord") != request.form.get("wachtwoord-confirmatie"):
+            return apology("De bevestiging komt niet overeen met het wachtwoord.")
+
+        # hash the password
+        wachtwoord = pwd_context.hash(request.form.get("wachtwoord"))
+
+        # insert the user into the database
+        result = db.execute("INSERT INTO gebruikers (gebruikersnaam, wachtwoord, email) VALUES \
+                            (:gebruikersnaam, :wachtwoord, :email)",
+                            gebruikersnaam=request.form.get("gebruikersnaam"), wachtwoord=wachtwoord,
+                            email=request.form.get("email"))
+
+        if not result:
+            return apology("username already exists")
+
+        # query database for username
+        rows = db.execute("SELECT * FROM gebruikers WHERE gebruikersnaam = :gebruikersnaam",
+                          gebruikersnaam=request.form.get("gebruikersnaam"))
+
+        # remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # redirect user to home page
+        return redirect(url_for("index"))
+
+    # else if user reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("registreren.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in."""
+
+    # forget any user_id
+    session.clear()
+
+    # if user reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # ensure username was submitted
+        if not request.form.get("gebruiker-inloggen"):
+            return apology("Geef een gebruikersnaam op")
+
+        # ensure password was submitted
+        elif not request.form.get("wachtwoord-inloggen"):
+            return apology("Geef een wachtwoord op")
+
+        # query database for username
+        rows = db.execute("SELECT * FROM gebruikers WHERE gebruikersnaam = :gebruikersnaam",
+                          gebruikersnaam=request.form.get("gebruiker-inloggen"))
+
+        rows1 = db.execute("SELECT * FROM gebruikers WHERE email = :email",
+                          email=request.form.get("gebruiker-inloggen"))
+
+        print(rows)
+        print(rows1)
+
+        if len(rows) == 0:
+            if len(rows1) != 1 or not pwd_context.verify(request.form.get("wachtwoord-inloggen"), rows1[0]["wachtwoord"]):
+                return apology("Ongeldige email en/of wachtwoord")
+
+        # ensure username exists and password is correct
+        elif len(rows) != 1 or not pwd_context.verify(request.form.get("wachtwoord-inloggen"), rows[0]["wachtwoord"]):
+            return apology("Ongeldige gebruikersnaam en/of wachtwoord")
+
+        # remember which user has logged in
+        if len(rows) > 0:
+            session["user_id"] = rows[0]["id"]
+
+        elif len(rows1) > 0:
+            session["user_id"] = rows1[0]["id"]
+
+        # redirect user to home page
+        return redirect(url_for("index"))
+
+    # else if user reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("inloggen.html")
+
+@app.route("/logout")
+def logout():
+    """Log user out."""
+
+    # forget any user_id
+    session.clear()
+
+    # redirect user to login form
+    return redirect(url_for("login"))
+
+@app.route("/vriend")
+@login_required
+def vriend():
+    return render_template("vriendtoevoegen.html")
+
+@app.route("/vriendenlijst")
+@login_required
+def vriendenlijst():
+    return render_template("vriendenlijst.html")
+
+# ZOEKEN
+'''
+    from urllib.request import urlopen
+    popular = json.loads(str((requests.get("https://api.themoviedb.org/3/discover/movie?api_key=9c226374f10b2dcd656cf7c348ee760a&language=nl&sort_by=popularity.desc&page=1&with_original_language=nl").content).decode('UTF-8')))
+    popular_results = popular["results"]
+'''
+@app.route("/zoeken", methods=["GET", "POST"])
+def zoekresultaat():
+
+    if request.method == "POST":
+        zoekterm = request.form.get("zoekterm")
+        if not zoekterm:
+            return apology("Geen zoekterm")
+        from urllib.request import urlopen
+        zoekresultaten = (json.loads(str((requests.get("https://api.themoviedb.org/3/search/movie?api_key=9c226374f10b2dcd656cf7c348ee760a&language=nl&query=" + zoekterm + "&include_adult=false&page=1").content).decode('UTF-8'))))["results"]
+        if not zoekresultaten:
+            return apology("Geen zoekresultaten")
+        else: return render_template("zoekresultaten.html", zoekresultaten=zoekresultaten)
+        ##als het nederlands is
+        #"original_language":"nl"
+
+
+ #       if not stockinfo:
+ #           return apology("Stock is not valid")
+ #       return render_template("stockprice.html", aandeel=stockinfo)
+ #   else:
+ #       return render_template("quote.html")
+
+    # zoekresultaat TMDb id naar IMDb id (als OMDb input)
+    "https://api.themoviedb.org/3/movie/569050?api_key=9c226374f10b2dcd656cf7c348ee760a&language=nl"
